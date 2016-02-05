382be72e2cc6bf07249ef33e8a8f703589103e24
Florian Rüberg <florian.rueberg@gmail.com>
2016-02-05 11:06:12 +0100

    Additional Services

--- tools/dev-webpack-server.js
+++ tools/dev-webpack-server.js
@@ -3,13 +3,26 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.conf.js')('dev');
 
-new WebpackDevServer(webpack(config), {
+var server = new WebpackDevServer(webpack(config), {
 	publicPath: config.output.publicPath,
 	hot: true,
 	historyApiFallback: true
-}).listen(5000, 'localhost', function (err) {
+});
+
+server.app.get('/hello', function( req, res ) {
+	res.send('Hello World!');
+});
+
+var l = server.app._router.stack.length;
+var e = server.app._router.stack[l - 1];
+server.app._router.stack.splice(l - 1, 1);
+server.app._router.stack.splice(4, 0, e);
+
+server.listen(5000, 'localhost', function (err) {
     if (err) {
       console.log(err);
     }
     console.log('Listening at localhost:3000');
 });
+
+
